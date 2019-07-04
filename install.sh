57f2cd7007ada69fbcf4c75854dfa1d0cb3e805c
Razz Fox <razzintown@gmail.com>
2019-07-04 13:49:57 -0700

    add android fix

--- install.sh
+++ install.sh
@@ -4,3 +4,11 @@ pip install --upgrade youtube_dl
 brew install ffmpeg
 
 npm install
+
+####
+# Fix for android
+ytdl="/data/data/com.termux/files/home/electron-gifs/node_modules/youtube-dl"
+test -d $ytdl && mkdir ${ytdl}/bin/
+test -d $ytdl && echo {\"path\":\"$(which youtube-dl)\"} > ${ytdl}/bin/details
+
+# also remove electron dependencies from package.json before running npm install
